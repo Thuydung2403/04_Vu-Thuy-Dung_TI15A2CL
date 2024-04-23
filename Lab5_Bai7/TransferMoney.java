09586ab7857fb16d089a2f3abd5a5fba8c28e1d0
Thuydung2403 <145429099+Thuydung2403@users.noreply.github.com>
2024-04-23 11:05:44 +0700

    Add files via upload

---
+++ Lab5_Bai7/TransferMoney.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab5_Bai7;
+
+import java.util.Random;
+
+/**
+ *
+ * @author dvu73
+ */
+public class TransferMoney implements Runnable {
+     Bank bank; 
+    int fromAcc; 
+    double maxAmount; 
+    int delay = 1000; 
+    public TransferMoney(Bank bank, int fromAcc, double maxAmount) 
+    { 
+        this.bank = bank; 
+        this.fromAcc = fromAcc; 
+        this.maxAmount = maxAmount; 
+    }
+    
+    @Override
+    public void run()  
+    { 
+        Random rd = new Random(); 
+        int toAcc = 0; 
+        double amount = 0; 
+        try 
+        { 
+            while (true)  
+            {             
+                do 
+                { 
+                    toAcc = rd.nextInt(bank.size()); 
+                }while (toAcc==fromAcc); 
+                amount = rd.nextInt((int)maxAmount); 
+                bank.transfer(fromAcc, toAcc, amount); 
+                Thread.sleep(rd.nextInt(delay)); 
+            } 
+        } 
+        catch(InterruptedException ex) 
+        { 
+            System.err.println("Giao dịch chuyển tiền từ account " + fromAcc + " sang account " + toAcc + " bị gián đoạn");
+            Thread.currentThread().interrupt();
+        } 
+    }
+}
